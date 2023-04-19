7fb3f7eb180841e59079ffadc920a82a4a2e97a3
YuChiungHou <b10956004@mail.npust.edu.tw>
2023-04-19 13:29:09 +0800

    fix insert cowinformation

--- fCowId/cowInformation/cowInformation_Insert.php
+++ fCowId/cowInformation/cowInformation_Insert.php
@@ -9,21 +9,23 @@ if (isset($_POST['submit'])) {
     $mid = $_POST['mid']; //母親牛編號
     $fid = $_POST['fid']; //精液編號
     //出生胎次、胎距計算
-    $sql = "SELECT * FROM `cows_information` WHERE mid='$mid'";
-    echo $sql;
-    $result = mysqli_query($db_link, $sql);
-    $count = mysqli_num_rows($result);
-    if ($count == 0||$count==NULL) {
-        $birthParity = 1;
-        $calvingInterval = "0天";
-    } else {
-        $birthParity = $count + 1;
-        $sql = "SELECT * FROM `cows_information` WHERE mid='$mid' AND birthParity='$count'";
-        $result = mysqli_query($db_link, $sql);
-        $row = mysqli_fetch_array($result);
-        $preDob = $row['dob'];
-        $calvingInterval = abs(floor((strtotime($preDob) - strtotime($dob)) / (60 * 60 * 24))).'天';
-    }
+    // $sql = "SELECT * FROM `cows_information` WHERE mid='$mid'";
+    // echo $sql;
+    // $result = mysqli_query($db_link, $sql);
+    // $count = mysqli_num_rows($result);
+    // if ($count == 0||$count==NULL) {
+    //     $birthParity = 1;
+    //     $calvingInterval = "0天";
+    // } else {
+    //     $birthParity = $count + 1;
+    //     $sql = "SELECT * FROM `cows_information` WHERE mid='$mid' AND birthParity='$count'";
+    //     $result = mysqli_query($db_link, $sql);
+    //     $row = mysqli_fetch_array($result);
+    //     $preDob = $row['dob'];
+    //     $calvingInterval = abs(floor((strtotime($preDob) - strtotime($dob)) / (60 * 60 * 24))).'天';
+    // }
+    $calvingInterval = "0天";
+    $birthParity = 0;
     $area = $_POST['area']; //區域
 
     $query = "INSERT INTO `cows_information`(`id`, `dob`, `mid`, `fid`, `birthParity`, `calvingInterval`, `area`) VALUES ('$id','$dob','$mid','$fid','$birthParity','$calvingInterval','$area')";
