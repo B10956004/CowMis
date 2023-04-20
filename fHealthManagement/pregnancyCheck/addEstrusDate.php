d4a54c61c4dead060f41b111a0ae0ff2afe38fa9
YuChiungHou <b10956004@mail.npust.edu.tw>
2023-04-20 18:16:26 +0800

    auto update birthParity

--- fHealthManagement/pregnancyCheck/addEstrusDate.php
+++ fHealthManagement/pregnancyCheck/addEstrusDate.php
@@ -21,15 +21,30 @@ if (isset($_POST['estrusDate'])) {
     $estrusDate = $_POST['estrusDate'];
     $query = "SELECT * FROM `pregnancy_check` WHERE id='$id' AND pregnancyresult IS NULL OR pregnancyresult= '' ";
     $result = mysqli_query($db_link, $query);
-    if (mysqli_num_rows($result) == 0) {
-        $query = "INSERT INTO `pregnancy_check`(`id`, `estrusdate`) VALUES('$id','$estrusDate')";
+    if (mysqli_num_rows($result) == 0) {//有無正在進行的懷孕期
+
+        $search="SELECT * FROM `pregnancy_check` WHERE id='$id' AND events!='空胎' ORDER BY sn DESC LIMIT 1";//找有無歷史資料，取最新一筆
+        $searchResult=mysqli_query($db_link,$search);
+        if(mysqli_num_rows($searchResult)!=0){
+            $rows=mysqli_fetch_array($searchResult);
+            $birthparity=$rows['birthparity']+1;
+        }else{
+            $birthparity=1;
+        }
+
+        $updateMotherQuery = "UPDATE `cows_information` SET `birthParity`='{$rows['birthparity']}' WHERE `id`='$id'";
+        mysqli_query($db_link, $updateMotherQuery);
+
+        $query = "INSERT INTO `pregnancy_check`(`id`, `estrusdate`,`birthParity`) VALUES('$id','$estrusDate','$birthparity')";
         $result = mysqli_query($db_link, $query);
         if ($result) {
             header("location:pregnancyCheck.php");
         } else {
             echo 'Please Check Your Query';
         }
+
     } else {
+        //更新資料
         $row=mysqli_fetch_array($result);
         $sn=$row['sn'];
         $query = "UPDATE `pregnancy_check` SET `estrusdate`='$estrusDate' WHERE `sn`='$sn' AND `id`='$id'";
