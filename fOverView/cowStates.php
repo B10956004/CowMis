37396e8fdca0dec8586604b38803a87244481651
YuChiungHou <b10956004@mail.npust.edu.tw>
2023-04-23 00:47:12 +0800

    add fontawsome script

--- fOverView/cowStates.php
+++ fOverView/cowStates.php
@@ -14,6 +14,9 @@
             text-align: center;
         }
     </style>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 
 <body>
@@ -29,7 +32,7 @@
         echo "<tr>";
         $sn = $row['sn'];
         $id = $row['id'];
-        echo "<td>狀態</td>";
+        echo "<td><i class=\"fas fa-circle\" style=\"color: green;\"></i></td>";
         echo "<td><a href=\"../fCowId/cowInformation/cowInformation.php?GetID=$id\" target=\"frame\">$id</a></td>";
         echo "</tr>";
     }
