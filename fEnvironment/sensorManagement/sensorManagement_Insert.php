f26410ad968fbaac832abf56e0fc93187ee01045
YuChiungHou <b10956004@mail.npust.edu.tw>
2023-08-09 11:29:38 +0800

    let 未連接 to 正常 for testing

--- fEnvironment/sensorManagement/sensorManagement_Insert.php
+++ fEnvironment/sensorManagement/sensorManagement_Insert.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     $cid = $_POST['cid']; //牛編號
     $uuid = $_POST['uuid']; //感測器編號
     $model = $_POST['model']; //型號
-    $query = "INSERT INTO `sensor_management`(`uuid`, `model`, `states`, `cid`) VALUES ('$uuid','$model','未連接','$cid')";
+    $query = "INSERT INTO `sensor_management`(`uuid`, `model`, `states`, `cid`) VALUES ('$uuid','$model','正常','$cid')";
     $result = mysqli_query($db_link, $query);
     if ($result) {
         header("location:sensorManagement.php");
