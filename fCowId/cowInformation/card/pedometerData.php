7c5353e7b00ae323c69aec2b15d8d9feb2eb1ba2
YuChiungHou <b10956004@mail.npust.edu.tw>
2023-10-04 01:55:51 +0800

    SQL fix

--- fCowId/cowInformation/card/pedometerData.php
+++ fCowId/cowInformation/card/pedometerData.php
@@ -18,12 +18,12 @@ rolling_avg as avg,
 rolling_std_dev as std_dev,
 Date(date) as date
 FROM (
-    SELECT id,value,date,
-        AVG(value) OVER (PARTITION BY id ORDER BY (Date(date) - CURDATE()) RANGE BETWEEN 1 PRECEDING AND CURRENT ROW) as rolling_avg,
-        STDDEV_POP(value) OVER (PARTITION BY id ORDER BY (Date(date) - CURDATE()) RANGE BETWEEN 1 PRECEDING AND CURRENT ROW)as rolling_std_dev
-    FROM (SELECT DISTINCT id, date, value FROM pedometer WHERE date BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW()) AS pedometer
+SELECT id,value,date,
+ AVG(value) OVER (PARTITION BY id ORDER BY (Date(date) - CURDATE()) RANGE BETWEEN 1 PRECEDING AND CURRENT ROW) as rolling_avg,
+ STDDEV_POP(value) OVER (PARTITION BY id ORDER BY (Date(date) - CURDATE()) RANGE BETWEEN 1 PRECEDING AND CURRENT ROW) as rolling_std_dev
+FROM (SELECT DISTINCT id, date, value FROM pedometer WHERE date BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW()) AS pedometer
 ) AS sub_query
-WHERE id='$id' GROUP by id,Date(date)");
+WHERE id='05Q119' GROUP by id,Date(date), rolling_avg, rolling_std_dev");
 while($row=$resultRolling->fetch_assoc()){
     $data[1][]=[
         'avg'=>$row['avg'],
